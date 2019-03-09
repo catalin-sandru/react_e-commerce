f43d99a7ff5dd64f90e03081eab2c990367aee81
Catalin Sandru <catalin.sandru88@gmail.com>
2019-03-09 13:16:17 +0000

    created title component and passed props to it from product list component

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,12 +1,13 @@
 import React, { Component }from 'react';
 import { Link } from 'react-router-dom';
-import logo from '../logo.svg'
-import { ButtonContainer } from './ButtonComponent'
+import logo from '../logo.svg';
+import { ButtonContainer } from './ButtonComponent';
+import styled from 'styled-components'
 
 class Navbar extends Component {
   render() {
     return(
-      <nav className="navbar navbar-expand-sm bg-primary navbar-dark px-sm-5">
+      <NavWrapper className="navbar navbar-expand-sm navbar-dark px-sm-5">
         <Link to="/">
           <img className="navbar-brand" src={logo} alt="store"/>
         </Link>
@@ -26,9 +27,18 @@ class Navbar extends Component {
           </ButtonContainer>
         </Link>
 
-      </nav>
+      </NavWrapper>
     )
   }
 }
+
+const NavWrapper = styled.nav`
+  background: var(--mainBlue);
+
+  .nav-link {
+    color: var(--mainWhite)!important;
+    font-size: 1.3rem;
+  }
+`
 
 export default Navbar
