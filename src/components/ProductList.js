d75936e3080756c2685c69b3a508e6027958a2ef
Catalin Sandru <catalin.sandru88@gmail.com>
2019-03-09 14:42:53 +0000

    return each object from the API with map method

--- src/components/ProductList.js
+++ src/components/ProductList.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react'
 import Title from './Title';
 import { storeProducts } from '../data'
 import { ProductConsumer } from '../context';
+import Product from './Product';
 
 export default class ProductList extends Component {
   state = {
@@ -16,7 +17,9 @@ export default class ProductList extends Component {
             <div className="row">
               <ProductConsumer>
                 {value => {
-                  console.log(value)
+                  return value.products.map(product => {
+                    return <Product product={product} key={product.id}/>
+                  })
                 }}
               </ProductConsumer>
             </div>
