840741959f9cf424bdc1e5c13a41171c650516c8
Catalin Sandru <catalin.sandru88@gmail.com>
2019-03-09 12:12:44 +0000

    added cart icon with bootstrap into button

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -5,9 +5,21 @@ import logo from '../logo.svg'
 class Navbar extends Component {
   render() {
     return(
-      <nav className="navbar navbar-expand-sm navbar-dark px-sm-5">
+      <nav className="navbar navbar-expand-sm bg-primary navbar-dark px-sm-5">
         <Link to="/">
           <img className="navbar-brand" src={logo} alt="store"/>
+        </Link>
+
+        <ul className="navbar-nav align-items-center">
+          <li className="nav-item-ml-5">
+            <Link to="/" className="nav-link">Products</Link>
+          </li>
+        </ul>
+
+        <Link to="/cart" className="ml-auto">
+          <button>
+            <i className="fas fa-cart-plus"/>
+          </button>
         </Link>
 
       </nav>
