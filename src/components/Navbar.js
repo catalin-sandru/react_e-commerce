268eac7ae7c742620c8163c0a2f27043c605fad3
Catalin Sandru <catalin.sandru88@gmail.com>
2019-03-09 12:03:02 +0000

    added image from svg with link

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,11 +1,16 @@
-import React, { Component }from 'react'
+import React, { Component }from 'react';
+import { Link } from 'react-router-dom';
+import logo from '../logo.svg'
 
 class Navbar extends Component {
   render() {
     return(
-      <div>
-        navbar
-      </div>
+      <nav className="navbar navbar-expand-sm navbar-dark px-sm-5">
+        <Link to="/">
+          <img className="navbar-brand" src={logo} alt="store"/>
+        </Link>
+
+      </nav>
     )
   }
 }
