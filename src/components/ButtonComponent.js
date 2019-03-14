46c858ded9dd76558f54a8593178d7568e955111
Catalin Sandru <catalin.sandru88@gmail.com>
2019-03-14 12:57:27 +0000

    used same prop for color, background and on hover

--- src/components/ButtonComponent.js
+++ src/components/ButtonComponent.js
@@ -6,14 +6,14 @@ font-size: 1.4rem;
 background: transparent;
 border: 0.2rem solid var(--lightBlue);
 border-color: ${props => props.cart ? "var(--mainYellow)" : "var(--lightBlue)"};
+color: ${props => props.cart ? "var(--mainYellow)" : "var(--lightBlue)"};
 border-radius: 0.5rem;
 padding: 0.2rem 0.5rem;
 margin: 0.2rem 0.5rem 0.2rem 0rem;
 transition: all 0.2s ease-in-out;
-color: var(--lightBlue);
 
 &:hover {
-  background: var(--lightBlue);
+  background: ${props => props.cart ? "var(--mainYellow)" : "var(--lightBlue)"};
   color: var(--mainDark);
 }
 `
