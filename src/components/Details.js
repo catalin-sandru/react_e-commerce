82d71dcf0cfee08d8616877c52000aadaf8e8764
Catalin Sandru <catalin.sandru88@gmail.com>
2019-03-14 12:08:54 +0000

    added buttons to details with mutable labels

--- src/components/Details.js
+++ src/components/Details.js
@@ -8,21 +8,45 @@ export default class Details extends Component {
     return (
       <ProductConsumer>
         {(value) => {
-          console.log(value.details)
           const {id, company, img, info, price, title, inCart} = value.details
           return (
             <div className="container py-5">
+            {/* title */}
               <div className="row">
                 <div className="col-10 mx-auto text-center text-slanted text-blue my-5">
                   <h1>{title}</h1>
                 </div>
               </div>
+              {/* title end */}
+              {/* product info */}
               <div className="row">
                 <div className="col-10 mx-auto col-md-6 my-3">
                   <img src={img} alt="product image" className="img-fluid"/>
                 </div>
+              {/* product text */}
                 <div className="col-10 mx-auto col-md-6 my-3 text-capitalize">
-                  <h1>title: {title}</h1>
+                  <h2>model: {title}</h2>
+                  <h4 className="text-title text-uppercase text-muted mt-3 mb-2">
+                    Made By: {company}
+                  </h4>
+                  <h4 className="text-blue">
+                    <strong>
+                      Price: £ {price}
+                    </strong>
+                  </h4>
+                  <p className="text-capitalize font-weight-bold mt-3 mb-0">
+                    some info about the product:
+                  </p>
+                  <p className="text-muted lead">{info}</p>
+                  {/* buttons */}
+                  <div>
+                    <Link to="/">
+                      <ButtonContainer>back to products</ButtonContainer>
+                    </Link>
+                    <ButtonContainer disabled={inCart ? true : false}>
+                      {inCart ? 'In Cart' : 'Add to Cart'}
+                    </ButtonContainer>
+                  </div>
                 </div>
               </div>
             </div>
