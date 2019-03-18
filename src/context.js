8aed8f84a6664b9a18e2792fd40bd9cf408ea7ca
Catalin Sandru <catalin.sandru88@gmail.com>
2019-03-18 19:22:23 +0000

    started cart component and rendered the title

--- src/context.js
+++ src/context.js
@@ -12,6 +12,9 @@ class ProductProvider extends Component {
     cart: [],
     modal: false,
     modalProduct: detailProduct,
+    cartSubTotal: 0,
+    cartTax: 0,
+    cartTotal: 0
   };
 
   
@@ -69,6 +72,22 @@ class ProductProvider extends Component {
     }
   )}
 
+  increment = id => {
+    console.log('increment')
+  }
+
+  decrement = id => {
+    console.log('decrement')
+  }
+
+  removeItem = id => {
+    console.log('removeItem')
+  }
+
+  clearCart = () => {
+    console.log('cart cleared')
+  }
+
   render() {
     return (
       <ProductContext.Provider value={
@@ -77,7 +96,11 @@ class ProductProvider extends Component {
           handleDetail: this.handleDetail,
           addToCart: this.addToCart,
           openModal: this.openModal,
-          closeModal: this.closeModal
+          closeModal: this.closeModal,
+          increment: this.increment,
+          decrement: this.decrement,
+          removeItem: this.removeItem,
+          clearCart: this.clearCart
         }
       }>
         {this.props.children}
